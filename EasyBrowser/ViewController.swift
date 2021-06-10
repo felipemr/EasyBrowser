98779730b7f70bd5ef3fffe04fb14712ca55b49b
Felipe Marques Ramos <felipemr94@outlook.com>
2021-06-10 12:10:45 -0300

    You can only access Apple and CNN urls

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -6,14 +6,92 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webview : WKWebView!
+    var progressView : UIProgressView!
+    
+    let websites = ["Apple.com","cnn.com"]
+    
+    override func loadView() {
+        webview = WKWebView()
+        webview.navigationDelegate = self
+        view = webview
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webview, action: #selector(webview.reload))
+        
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView )
+        
+        toolbarItems = [progressButton,spacer,refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webview.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil )
+        
+        
+        let url = URL(string: "https://" + websites[0])!
+        webview.load(URLRequest(url: url))
+        webview.allowsBackForwardNavigationGestures = true
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webview.title
     }
 
 
+    @objc func openTapped(){
+        let alertController = UIAlertController(title: "Open ", message: nil, preferredStyle: .actionSheet)
+        
+        for webSite in websites{
+            alertController.addAction(UIAlertAction(title: webSite, style: .default, handler: openPage))
+
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        alertController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(alertController, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webview.load(URLRequest(url: url))
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress"{
+           progressView.progress = Float(webview.estimatedProgress)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website.lowercased()) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        let alertController = UIAlertController(title: "Stop!", message: "Not allowed url tried to load! \n \(url!)", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok, sorry.", style: .cancel, handler: nil))
+        present(alertController, animated: true)
+        decisionHandler(.cancel)
+    }
 }
 
